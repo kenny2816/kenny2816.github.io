a59424be187968342c5bf937dc086a648d5a499f
Kenneth Wang <kennethwang732@gmail.com>
2024-07-14 15:38:23 -0400

    new tutorial restart

--- server.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const express = require("path");
-const app = express();
-app.get("/*", (req, res) => {
-    res.sendFile(path.resolve("frontend", "index.html"));
-})
